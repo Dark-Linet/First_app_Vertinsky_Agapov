e1fb59ee5a68c52c7d94dd7031965def108fbdd1
Dark-Linet(college) <maks.ver14@gmail.com>
2026-01-17 14:41:56 +0300

    Сделана первая функциональность приложения

--- app/src/main/java/com/example/first_app_vertinsky_agapov/MainActivity.kt
+++ app/src/main/java/com/example/first_app_vertinsky_agapov/MainActivity.kt
@@ -5,16 +5,34 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+import android.widget.TextView
+import androidx.core.view.WindowCompat
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val buttonRoll = findViewById<Button>(R.id.buttonRoll)
+        val resultTextView = findViewById<TextView>(R.id.resultTextView)
+
+        buttonRoll.setOnClickListener {
+            val diceResult = Random.nextInt(1, 7) // 1..6
+            resultTextView.text = "Выпало: $diceResult"
+        }
+
         }
     }
-}
+
+
+
+
+
+
+
+
+
+
