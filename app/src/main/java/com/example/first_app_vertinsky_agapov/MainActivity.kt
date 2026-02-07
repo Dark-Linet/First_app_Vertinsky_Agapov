5b3b488a059dec58d976cbed568b71763ca7bf0e
Dark-Linet(college) <maks.ver14@gmail.com>
2026-02-07 12:24:42 +0300

    commit

--- app/src/main/java/com/example/first_app_vertinsky_agapov/MainActivity.kt
+++ app/src/main/java/com/example/first_app_vertinsky_agapov/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.first_app_vertinsky_agapov
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.example.first_app_vertinsky_agapov.databinding.ActivityMainBinding
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.widget.Button
@@ -11,21 +12,50 @@ import androidx.core.view.WindowCompat
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
 
-        val buttonRoll = findViewById<Button>(R.id.buttonRoll)
-        val resultTextView = findViewById<TextView>(R.id.resultTextView)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        buttonRoll.setOnClickListener {
-            val diceResult = Random.nextInt(1, 7) // 1..6
-            resultTextView.text = "Выпало: $diceResult"
-        }
-
+        binding.buttonRoll.setOnClickListener {
+            val diceResult = Random.nextInt(1, 7)
+            binding.resultTextView.text = getString(R.string.result_format, diceResult)
         }
     }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      //  val buttonRoll = findViewById<Button>(R.id.buttonRoll)
+     //   val resultTextView = findViewById<TextView>(R.id.resultTextView)
+
+   //     buttonRoll.setOnClickListener {
+    //        val diceResult = Random.nextInt(1, 7) // 1..6
+     //       resultTextView.text = "Выпало: $diceResult"
+
+
 
 
 
